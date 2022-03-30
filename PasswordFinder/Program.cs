83a85e0a70c1190b39170e1b930500bbd7dc91bb
Marco131 <marco.rdrgs2@eduge.ch>
2022-03-30 07:02:29 +0200

    first commit

---
+++ PasswordFinder/Program.cs
@@ -0,0 +1,42 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+
+namespace PasswordFinder
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            string password = Console.ReadLine();
+            bool pwFound = false;
+
+            int currentLength = 1;
+            List<char> word = new List<char>();
+            while (!pwFound) // loop while the password hasn't been found
+            {
+                word.Clear();
+                while(word.Count < currentLength) // fill word with default value
+                {
+                    word.Add('a');
+                }
+
+                int letterNb = 0;
+                while(letterNb < currentLength) // loop for the number of letters in the word
+                {
+                    Console.WriteLine(new string(word.ToArray()));
+                    word[letterNb] += (char)1;
+
+                    if (word[letterNb] == 'z'+1)
+                    {
+                        word[letterNb] = 'a';
+                        letterNb += 1;
+                    }else
+                        letterNb = 0;
+                }
+
+                currentLength += 1;
+            }
+        }
+    }
+}
