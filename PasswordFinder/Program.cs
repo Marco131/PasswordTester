eb93c3efbd4aa531427437bd89c17408ff0157d8
Marco131 <marco.rdrgs2@eduge.ch>
2022-04-01 07:34:14 +0200

    bug resolution + visual improvement

--- PasswordFinder/Program.cs
+++ PasswordFinder/Program.cs
@@ -1,6 +1,7 @@
 ﻿using System;
 using System.Collections.Generic;
 using System.Linq;
+using System.Security;
 
 namespace PasswordFinder
 {
@@ -8,8 +9,31 @@ namespace PasswordFinder
     {
         static void Main(string[] args)
         {
-            string password = Console.ReadLine();
+
+            //string password = Console.ReadLine();
+            SecureString securePwd = new SecureString();
+            ConsoleKeyInfo key;
             bool pwFound = false;
+
+            // get password with password input
+            Console.Write("Insert your password : ");
+            do
+            {
+                key = Console.ReadKey(true);
+
+                // ignore any key out of range.
+                if (((int)key.Key) >= 65 && ((int)key.Key <= 90))
+                {
+                    // append the character to the password.
+                    securePwd.AppendChar(key.KeyChar);
+                    Console.Write("*");
+                }
+
+                // exit if Enter key is pressed.
+            } while (key.Key != ConsoleKey.Enter);
+
+            Console.WriteLine();
+            Console.WriteLine();
 
             int currentLength = 1;
             List<char> word = new List<char>();
@@ -25,14 +49,19 @@ namespace PasswordFinder
                 while(letterNb < currentLength) // loop for the number of letters in the word
                 {
                     Console.WriteLine(new string(word.ToArray()));
-                    word[letterNb] += (char)1;
 
-                    if (word[letterNb] == 'z'+1)
+                    if (word[letterNb] == 'z')
                     {
                         word[letterNb] = 'a';
                         letterNb += 1;
-                    }else
+                    }
+                    else
                         letterNb = 0;
+
+                    if (letterNb < word.Count)
+                    {
+                        word[letterNb] += (char)1;
+                    }
                 }
 
                 currentLength += 1;
