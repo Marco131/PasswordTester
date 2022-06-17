af1cd577d3cd7294127c6158b4f4c91a89fe4b1c
Marco131 <marco.rdrgs2@eduge.ch>
2022-06-18 00:04:03 +0200

    bash version, check if character is in the list

--- bash/passwordTester.sh
+++ bash/passwordTester.sh
@@ -1,19 +1,51 @@
 #!/bin/bash
 clear
 
+# read character file into array
+readarray -t pwCharArray < characters.txt
+
+# function that checks if a character is in pwCharArray
+# Argument: character to test
+# Returns: 1 if the array contains the char, else 0
+isCharInArray () {
+	local charToTest="$1"
+
+	for item in "${pwCharArray[@]}"
+	do
+		if [[ "$item" = "$charToTest"* ]]; then
+		return  1
+		fi
+	done
+
+	return 0
+}
+
 # password input
 HASPRESSEDENTER=false
 PASSWORD=""
-echo -n "write : "
+echo -n "Enter a password : "
 until $HASPRESSEDENTER; do
 	read -s -n1 char
 
-	if [[ $char = "" ]]; then
-	HASPRESSEDENTER=true
+	if [[ $char = "" ]]; then	# if char is enter 
+		HASPRESSEDENTER=true
+	elif [[ $char = $'\177' ]] && [ ! -z $PASSWORD ]; then	# if char is backspace
+		PASSWORD=${PASSWORD::-1} # remove last character from password
+
+		# rewrite input
+		clear
+		echo -n "Enter a password : "
+		echo $PASSWORD | awk -v ORS="" '{ gsub(/./,"&\n") ; print }' | \
+		while read char
+		do
+			echo -n "*"
+		done
 	else
-	echo -n "*"
-	PASSWORD+=$char
+		isCharInArray "$char"
+		if [ $? -eq 1 ]; then
+			echo -n "*"
+			PASSWORD+=$char
+		fi
 	fi
 done
 echo ""
-echo $PASSWORD
